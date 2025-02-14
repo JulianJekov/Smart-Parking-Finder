c004e1ccbdaf2e11a5d5886223d1b244a1326f98
Julian Jekov <drmsz@abv.bg>
2025-02-14 21:41:03 +0200

    Add Custom user details service

---
+++ src/main/java/com/smart/parking/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.smart.parking.service.impl;
+
+import com.smart.parking.model.entity.User;
+import com.smart.parking.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
