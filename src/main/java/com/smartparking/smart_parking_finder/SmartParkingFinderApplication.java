da770a52fce265baf71205b34f6a1a3de7e87178
Julian Jekov <drmsz@abv.bg>
2025-02-14 11:36:36 +0200

    Initial commit

---
+++ src/main/java/com/smartparking/smart_parking_finder/SmartParkingFinderApplication.java
@@ -0,0 +1,13 @@
+package com.smartparking.smart_parking_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartparking/smart_parking_finder/SmartParkingFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartparking.smart_parking_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
