543517351ef44d2c9f1c387d8e31c3538c174659
Julian Jekov <drmsz@abv.bg>
2025-02-14 12:55:34 +0200

    Add basic spring security

---
+++ src/main/java/com/smart/parking/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.smart.parking.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll()
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
