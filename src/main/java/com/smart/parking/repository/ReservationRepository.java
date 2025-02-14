20cb077de1612bdaf7ebf38e6e4a9550305c8fbf
Julian Jekov <drmsz@abv.bg>
2025-02-14 12:10:49 +0200

    Add repositories

---
+++ src/main/java/com/smart/parking/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.smart.parking.repository;
+
+import com.smart.parking.model.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByUserId(Long userId);
+}
---
+++ src/main/java/com/smart/parking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.smart.parking.repository;
+
+import com.smart.parking.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
