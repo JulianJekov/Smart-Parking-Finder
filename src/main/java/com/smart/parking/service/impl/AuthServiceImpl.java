adb4ea1192ed807fe239003c296ff456fe2389e5
Julian Jekov <drmsz@abv.bg>
2025-02-14 21:44:38 +0200

    Add AuthServiceImpl

--- src/main/java/com/smart/parking/service/impl/AuthServiceImpl.java
+++ src/main/java/com/smart/parking/service/impl/AuthServiceImpl.java
@@ -1,4 +1,34 @@
 package com.smart.parking.service.impl;
 
-public class AuthServiceImpl {
+import com.smart.parking.model.dto.auth.UserRegisterDTO;
+import com.smart.parking.model.entity.User;
+import com.smart.parking.model.enums.Role;
+import com.smart.parking.repository.UserRepository;
+import com.smart.parking.service.AuthService;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void registerUser(UserRegisterDTO userRegisterDTO) {
+        if (userRepository.findByEmail(userRegisterDTO.getEmail()) != null) {
+            throw new IllegalArgumentException("Email address already in use");
+        }
+        User user = modelMapper.map(userRegisterDTO, User.class);
+        user.setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+    }
 }
