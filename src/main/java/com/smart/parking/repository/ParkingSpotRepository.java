3540ac8a42389c154a8499c0b20ac0b983d44c5a
Julian Jekov <drmsz@abv.bg>
2025-02-19 21:56:47 +0200

    Add find by owner id in parking spot repository

--- src/main/java/com/smart/parking/repository/ParkingSpotRepository.java
+++ src/main/java/com/smart/parking/repository/ParkingSpotRepository.java
@@ -1,5 +1,6 @@
 package com.smart.parking.repository;
 
+import com.smart.parking.model.dto.parkingSpot.ParkingSpotViewDTO;
 import com.smart.parking.model.entity.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
     List<ParkingSpot> findByAvailableSpotsGreaterThan(int availableSpots);
+    List<ParkingSpot> findByOwnerId(Long ownerId);
 }
